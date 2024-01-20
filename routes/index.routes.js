18990c7021343543d7d26b795b5adf3a8ed6ba12
2152001 <tokaabdelgawad60@gmail.com>
2024-01-20 17:02:47 +0200

    cart

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,12 @@
 import { globalError } from "../middleware/globalError.js"
 import { AppError } from "../utils/AppError.js"
 import  router from "../routes/client.js"
-
+import cartRouter from './cart.js'
 export const apiRoutes =(app)=>{
 
 
     app.use(router)
+    app.use(cartRouter)
 
     //------------------------------
     app.use('*',(req,res,next)=>{
