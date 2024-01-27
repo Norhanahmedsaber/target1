66ac7bb2612da19a76a33c5d4b763dcc22676978
Norhanahmedsaber <norhan3182001@gmail.com>
2024-01-27 14:52:53 +0200

    ..

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,14 +1,14 @@
 import { globalError } from "../middleware/globalError.js"
 import { AppError } from "../utils/AppError.js"
 import orderRouter from './Order.js'
-import cartRouter from './cart.js'
+///import cartRouter from './cart.js'
 import userRouter from "../routes/user.js"
 import productRouter from "./product.js"
 import categoryRouter from "./category.js"
 export const apiRoutes =(app)=>{
 
     app.use(orderRouter)
-    app.use(cartRouter)
+ //   app.use(cartRouter)
     app.use(userRouter)
     app.use('/products',productRouter)
     app.use('/category',categoryRouter)
