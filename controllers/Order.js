c26a7cbd6b0c7a59d487a20fe4c7881992f5eee5
2152001 <tokaabdelgawad60@gmail.com>
2024-01-27 09:32:38 +0200

    final update

--- controllers/Order.js
+++ controllers/Order.js
@@ -1,4 +1,4 @@
-import Order from '../Models/Order.js';  
+import Order from "../Models/Order.js";
 
 const createOrder = async (req, res) => {
   try {
@@ -15,40 +15,8 @@ const createOrder = async (req, res) => {
     res.status(201).json(savedOrder);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-export default{ createOrder };
-
-// import order from '../Models/Order.js'
-// import generateErrorMessage from '../utils/generateErrorMessage.js'
-
-// // async function getOrders(){
-// //     try{
-// //         let myorders = await await order.find().populate({
-// //             path:"items.productId",
-// //             select:"title price total"
-// //         })
-// //         if(!myorders)
-// //         {
-// //             generateErrorMessage(400 , "There is no orders. ")
-// //         }
-// //         if(myorders.items.length == 0)
-// //         {
-// //             myorders.items = []
-// //             myorders.subTotal=0
-// //             const data = await myorders.save()
-// //             return {value:data}
-// //         }  
-// //         return {value:myorders}
-// //     }catch(err)
-// //     {
-// //         return (err.message)
-// //     }
-// // }
-
-// export default {
-//     getOrders,
-//     removeOrderItem
-// }
+export default { createOrder };
